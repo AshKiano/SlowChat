5056e6d41736728d2d2327ddacc70d3ec76e3b0b
jvyskocil <ash.kiano@gmail.com>
2024-07-01 21:06:04 +0200

    version 1.3

--- src/main/java/com/ashkiano/slowchat/SlowChat.java
+++ src/main/java/com/ashkiano/slowchat/SlowChat.java
@@ -9,7 +9,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 public class SlowChat extends JavaPlugin implements Listener, CommandExecutor {
@@ -24,6 +29,7 @@ public class SlowChat extends JavaPlugin implements Listener, CommandExecutor {
         loadDelayFromConfig();
         Metrics metrics = new Metrics(this, 21924);
         this.getLogger().info("Thank you for using the SlowChat plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+        checkForUpdates();
     }
 
     @Override
@@ -88,5 +94,45 @@ public class SlowChat extends JavaPlugin implements Listener, CommandExecutor {
 
     private boolean canSendMessage(String playerId, long currentTime) {
         return !lastMessageTime.containsKey(playerId) || (currentTime - lastMessageTime.get(playerId) >= delayInSeconds * 1000);
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://plugins.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 }
